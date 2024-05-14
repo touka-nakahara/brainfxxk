2bfe990c746d173e41da37ae84c32dbab6ddd702
touka-nakahara <167406123+touka-nakahara@users.noreply.github.com>
2024-05-14 11:47:42 +0900

    とりあえず動く

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	brainfxxk "brainfxxk/interpriter"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	// 入力受け取り
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Println("Usage: arg - [filename] ")
+		return
+	}
+
+	//TODO try catchに変更
+	f, err := os.Open(args[1])
+
+	if err != nil {
+		fmt.Printf("os.Open: %s", err)
+		return
+	}
+
+	buf, err := io.ReadAll(f)
+	if err != nil {
+		fmt.Printf("io.ReadAll: %s", err)
+		return
+	}
+
+	fmt.Printf("%s", string(buf))
+
+	brainfxxk.Interpreter(string(buf))
+}
