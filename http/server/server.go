542d5f15a72fc90a8852a1e801cfaef92427931e
touka-nakahara <167406123+touka-nakahara@users.noreply.github.com>
2024-05-16 10:11:26 +0900

    add HTTP error handling

--- http/server/server.go
+++ http/server/server.go
@@ -20,15 +20,17 @@ func BrainfxxkFuncPOST(w http.ResponseWriter, r *http.Request) {
 	//TODO ここで全部読み込んだ方がいい気がする (入力の読み込みのエラーとBrainFxxkの読み込みを分けたい)
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		//TODO Request Errorを返したい
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+		return
 	}
+
 	command := string(body)
 
 	// brainfxxkの起動
 	//TODO レスポンスとBFの起動は分けたい
 	result, err := brainfxxk.Run(command)
 	if err != nil {
-		//TODO 403を返す
+		http.Error(w, "Failed to interept", http.StatusForbidden)
 	}
 
 	// レスポンス返答(text/plain)
@@ -44,7 +46,6 @@ func Server() {
 	//TODO forループでhanderスライスを見て, ハンドル登録する
 	http.Handle("GET /", http.FileServer(http.Dir("http/static/root")))
 	http.HandleFunc("POST /run", BrainfxxkFuncPOST)
-	// Methodが読めるらしい
 
 	err := s.ListenAndServe()
 
